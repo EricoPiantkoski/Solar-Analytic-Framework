e922971ce0f47b6705098cd7b0b05a2973b92479
Erico Piantkoski <ericopiantkoski@gmail.com>
2020-05-18 15:06:22 -0400

    Alteração de client e server e arquivo renomeado testmain.py > init.py

--- clientdesk1.py
+++ clientdesk1.py
@@ -1,4 +1,5 @@
-import socket, requests, json, sys
+import socket, requests, json
+#import sys
 
 def conn(data):
     sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -7,8 +8,8 @@ def conn(data):
         sc.connect((ip, port))
         print('connection sucessful')
         for item in data:
-            i = sys.getsizeof(item)
-            print(i)
+            #i = sys.getsizeof(item)
+            #print(i)
             sc.sendall(bytes(item, 'utf-8'))
     except:
         print('connection uncessful. make sure the server is online')
@@ -16,7 +17,7 @@ def conn(data):
 
 #ip = '192.168.116.1'
 #ip = '113.167.9.63'
-ip = socket.gethostbyname(socket.gethostname())
+ip = socket.gethostbyname(socket.gethostname()) #ipserver
 port = 7352
 
 
@@ -27,12 +28,6 @@ region = r['region']
 c = r['coords'].split(',')
 lat = c[0]
 lon = c[1]
-
-#region = estadosigla(region)
-
 local = [lat, lon, city, region]
-
-#for item in local:
-#    print(item)
 
 conn(local)
