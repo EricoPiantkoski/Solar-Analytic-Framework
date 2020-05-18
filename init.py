3d10739d4e6f518b0dbfb86a404c0610836699bd
Erico Piantkoski <ericopiantkoski@gmail.com>
2020-05-18 15:11:48 -0400

    Add init.py

---
+++ init.py
@@ -0,0 +1,23 @@
+import main
+import csv
+import server
+
+# init server (needs ESP8266 client)
+local = server.local
+# corrigir estado
+
+#local = ['-15.085800', '-57.526000', 'Barra do Bugres', 'MT']
+# transformar em credencial:
+usr = 'ericopiantkoski@gmail.com'
+pas='01c7od42'
+
+obj = main.SA(float(local[0]), float(local[1]), local[2], local[3])
+
+#print(obj.dirBDMEP())
+#s = obj.dirBDMEP()
+#obj.wsBDMEP(usr, pas)
+#list = obj.bdmeptxttolist()
+#lista = obj.averageinsolation(list)
+#obj.im(lista)
+
+print(obj.red())
