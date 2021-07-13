434df7247c7bd5e742bdf2d96a32c81d0cf11843
Erico Piantkoski <ericopiantkoski@MacBook-Pro-de-Erico.local>
2021-07-13 14:47:42 -0400

    main tcp server edit

--- tcp-server/main.py
+++ tcp-server/main.py
@@ -20,33 +20,33 @@ if __name__ == '__main__':
 
     server.server(interface, port)
     ipHost = server.returnData()
-    '''
-    clientLocation = location.geolocation(ipHost[0])
-    clientLocation[3] = nearest_station.stateAbbreviation(clientLocation[3])
+    
+    # clientLocation = location.geolocation(ipHost[0])
+    # clientLocation[3] = nearest_station.stateAbbreviation(clientLocation[3])
         
 
-    #clientLocation = [-15.0725, -57.1811, 'Barra do Bugres', 'MT']
-    bdmepStations = nearest_station.dirBDMEP(clientLocation, dir_data)
+    # #clientLocation = [-15.0725, -57.1811, 'Barra do Bugres', 'MT']
+    # bdmepStations = nearest_station.dirBDMEP(clientLocation, dir_data)
 
-    while not historicinsolation:
-        bdmepFileName = searchData.searchDataStation(bdmepStations, counter, dir_data) 
-        historicinsolation = metricApplications.bdmepcsvtolist(bdmepFileName, dir_data)
-        counter += 1
+    # while not historicinsolation:
+    #     bdmepFileName = searchData.searchDataStation(bdmepStations, counter, dir_data) 
+    #     historicinsolation = metricApplications.bdmepcsvtolist(bdmepFileName, dir_data)
+    #     counter += 1
 
-    # average daily isolation (photoperiod for each day) [imD, day, month]
-    imd = metricApplications.averageinsolation(historicinsolation)
+    # # average daily isolation (photoperiod for each day) [imD, day, month]
+    # imd = metricApplications.averageinsolation(historicinsolation)
 
-    # List with 12 items corresponding to 12 months [total insolation Month (in hours), month]
-    im = metricApplications.im(imd)
+    # # List with 12 items corresponding to 12 months [total insolation Month (in hours), month]
+    # im = metricApplications.im(imd)
 
-    trm = metricApplications.trm(clientLocation, dir_data)
-    red = metricApplications.red(imd, im, trm)
+    # trm = metricApplications.trm(clientLocation, dir_data)
+    # red = metricApplications.red(imd, im, trm)
 
 
-    for item in red:
-        print(item)
+    # for item in red:
+    #     print(item)
 
-    '''
+    
 
 
 
