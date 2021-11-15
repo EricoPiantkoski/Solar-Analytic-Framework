0ba8ecc9c4c17b7f28ddceefd5da48d944e06f8a
Erico Piantkoski <ericopiantkoski@gmail.com>
2021-11-15 15:54:36 -0300

    avarage in compare script added

--- comparison.js
+++ comparison.js
@@ -17,70 +17,140 @@ function toGet(date, esp_id){
     return request.responseText
 }
 
+// function format_label_date(iterator){
+//     if (month == 5|| month == 7 || month == 10 || month ==12){
+//         if (iterator<0){
+//             if (Number(day)+iterator==0){
+//                 return '30'+'/'+add_left_zero((month-1))
+//             }else {
+//                 // return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
+//                 return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
+//             }
+//         }else if(iterator>0){
+//             if(Number(day)+iterator==32){
+//                 if (month == 12){
+//                     return '01/01'
+//                 }
+//                 return '01'+'/'+add_left_zero(month)
+//             }else {
+//                 return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
+//             }
+//         }
+//     }else if (month == 3){
+//         if (iterator<0){
+//             if (day+iterator==0){
+//                 if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))){
+//                     return '29'+'/'+add_left_zero((month-1))
+//                 }else{
+//                     return '28'+'/'+add_left_zero((month-1))
+//                 }
+//             }else {
+//                 return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
+//             }
+//         }else if(iterator>0){
+//             if(Number(day)+iterator==32){
+//                 return '01'+'/'+add_left_zero(month+1)
+//             }else {
+//                 return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
+//             }
+//         }
+//     }else if(month == 1 || month == 8){
+//         if (iterator<0){
+//             if (Number(day)+iterator==0){
+//                 return '31'+'/'+add_left_zero((month-1))
+//             }else {
+//                 return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
+//             }
+//         }else if(iterator>0){
+//             if(Number(day)+iterator==32){
+//                 return '01'+'/'+add_left_zero(month+1)
+//             }else {
+//                 return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
+//             }
+//         }
+//     }else{
+//         if (iterator<0){
+//             if (Number(day)+iterator==0){
+//                 return '31'+'/'+add_left_zero((month-1))
+//             }else {                
+//                 return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
+//             }
+//         }else if(iterator>0){
+//             if(Number(day)+iterator==31){
+//                 return '01'+'/'+add_left_zero(month+1)
+//             }else {                
+//                 return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
+//             }
+//         }
+//     }
+// }
+
 function format_label_date(iterator){
     if (month == 5|| month == 7 || month == 10 || month ==12){
-        if (iterator<0){
-            if (Number(day)+iterator==0){
-                return '30'+'/'+add_left_zero((month-1))
-            }else {
-                // return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
-                return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
-            }
-        }else if(iterator>0){
-            if(Number(day)+iterator==32){
-                if (month == 12){
-                    return '01/01'
-                }
-                return '01'+'/'+add_left_zero(month)
-            }else {
-                return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
-            }
+        if (Number(day)+iterator==-3){
+            return '27'+'/'+add_left_zero((month-1))
+        }else if (Number(day)+iterator==-2){
+            return '28'+'/'+add_left_zero((month-1))
+        }else if (Number(day)+iterator==-1){
+            return '29'+'/'+add_left_zero((month-1))
+        }else if (Number(day)+iterator==0){
+            return '30'+'/'+add_left_zero((month-1))
+        }else {
+            // return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
+            return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
         }
     }else if (month == 3){
-        if (iterator<0){
-            if (day+iterator==0){
-                if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))){
-                    return '29'+'/'+add_left_zero((month-1))
-                }else{
-                    return '28'+'/'+add_left_zero((month-1))
-                }
-            }else {
-                return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
+        if (day+iterator==-3){
+            if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))){
+                return '26'+'/'+add_left_zero((month-1))
+            }else{
+                return '25'+'/'+add_left_zero((month-1))
             }
-        }else if(iterator>0){
-            if(Number(day)+iterator==32){
-                return '01'+'/'+add_left_zero(month+1)
-            }else {
-                return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
+        }else if (day+iterator==-2){
+            if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))){
+                return '27'+'/'+add_left_zero((month-1))
+            }else{
+                return '26'+'/'+add_left_zero((month-1))
             }
+        }else if (day+iterator==-1){
+            if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))){
+                return '28'+'/'+add_left_zero((month-1))
+            }else{
+                return '27'+'/'+add_left_zero((month-1))
+            }
+        }else if (day+iterator==0){
+            if ((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0))){
+                return '29'+'/'+add_left_zero((month-1))
+            }else{
+                return '28'+'/'+add_left_zero((month-1))
+            }
+        }else {
+            return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
         }
+        
     }else if(month == 1 || month == 8){
-        if (iterator<0){
-            if (Number(day)+iterator==0){
-                return '31'+'/'+add_left_zero((month-1))
-            }else {
-                return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
-            }
-        }else if(iterator>0){
-            if(Number(day)+iterator==32){
-                return '01'+'/'+add_left_zero(month+1)
-            }else {
-                return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
-            }
+        if (Number(day)+iterator==-3){
+            return '28'+'/'+add_left_zero((month-1))
+        }else if (Number(day)+iterator==-2){
+            return '29'+'/'+add_left_zero((month-1))
+        }else if (Number(day)+iterator==-1){
+            return '30'+'/'+add_left_zero((month-1))
+        }else if (Number(day)+iterator==0){
+            return '31'+'/'+add_left_zero((month-1))
+        }else {
+            return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
         }
     }else{
-        if (iterator<0){
-            if (Number(day)+iterator==0){
-                return '31'+'/'+add_left_zero((month-1))
-            }else {                
-                return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
-            }
-        }else if(iterator>0){
-            if(Number(day)+iterator==31){
-                return '01'+'/'+add_left_zero(month+1)
-            }else {                
-                return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
-            }
+        if (Number(day)+iterator==-3){
+            return '28'+'/'+add_left_zero((month-1))
+        }else if (Number(day)+iterator==-2){
+            return '29'+'/'+add_left_zero((month-1))
+        }else if (Number(day)+iterator==-1){
+            return '30'+'/'+add_left_zero((month-1))
+        }else if (Number(day)+iterator==0){
+            return '31'+'/'+add_left_zero((month-1))
+        }else {                
+            return add_left_zero(Number(day)+iterator)+'/'+add_left_zero(month)
         }
     }
 }
@@ -90,31 +160,48 @@ function comparison(esp_id){
         data = toGet(f_now+'/'+year, esp_id)
         // data = toGet('17/09'+'/'+year, esp_id)
         today = JSON.parse(data)
+        average_spent = 0
+        count = 0
+        for(i = -3; i<=-1; i++){
+            olddata = toGet(format_label_date(i)+'/'+year, esp_id)
+            olddata = JSON.parse(olddata)
+            console.log('olddata: ', olddata.data.spent)
+            average_spent += olddata.data.spent
+            console.log('spent', average_spent)
+            count++ 
+        }
 
-        old_data = toGet(format_label_date(-1)+'/'+year, esp_id)
-        // old_data = toGet('16/09'+'/'+year, esp_id)
-        yesterday = JSON.parse(old_data)
-        console.log(yesterday)
+        average_spent /= count
+        console.log('average: ', average_spent)
+
+        // old_data = toGet(format_label_date(-1)+'/'+year, esp_id)
+        // yesterday = JSON.parse(old_data)
+        // console.log('yesterday: ', yesterday)
         
         estimated_gain = today.data.prediction * (today.data.eficience/100)
         console.log('estimated gain = ', estimated_gain)
 
-        console.log('old spent = ', yesterday.data.spent)
+        //console.log('old spent = ', yesterday.data.spent)
 
         console.log('estimated gain 120%', estimated_gain * 1.2)
         console.log('estimated gain 110%', estimated_gain * 1.1)
         console.log('estimated gain 100%', estimated_gain)
         console.log('estimated gain 90%', estimated_gain * 0.9)
         
-        if (yesterday.data.spent >= estimated_gain * 1.2){
-            document.write('Economia estremamente recomendada')
-        }else if(yesterday.data.spent >= estimated_gain * 1.1){
+        if (average_spent >= estimated_gain * 1.2){
+            console.log('data.spent >= 120% estimated_gain')
+            document.write('Economia extremamente recomendada')
+        }else if(average_spent >= estimated_gain * 1.1){
+            console.log('data.spent >= 110% estimated_gain')
             document.write('Economia fortemente recomendada')
-        }else if(yesterday.data.spent >= estimated_gain){
+        }else if(average_spent >= estimated_gain){
+            console.log('data.spent >= estimated_gain')
             document.write('Economia recomendada')
-        }else if(yesterday.data.spent >= estimated_gain * 0.9){
+        }else if(average_spent >= estimated_gain * 0.9){
+            console.log('data.spent >= 90% estimated_gain')
             document.write('Nível de energia aceitável')
         }else{
+            console.log('data.spent >= 80% estimated_gain')
             document.write('Nível de energia boa')
         }
     }catch{
